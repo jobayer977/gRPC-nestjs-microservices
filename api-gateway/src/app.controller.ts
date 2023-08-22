bdf22607d5477b23f0654c49d03b508a67fc665b
Jobayer <jobayerhossain977@gmail.com>
2023-08-22 15:14:20 +0600

    ini

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
-import { GrpcMethod } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
@@ -10,11 +9,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @GrpcMethod('UsersService', 'GetUser')
-  findOne(data) {
-    console.log(data);
-    return data;
   }
 }
